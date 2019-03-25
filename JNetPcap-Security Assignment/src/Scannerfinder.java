8d222eacc145e57104668d8d355a25daee8f5d87
Glen Johnson <glen.e.johnson@sbcglobal.net>
2019-03-25 17:37:42 -0600

    Started Assignment

--- JNetPcap-Security Assignment/src/Scannerfinder.java
+++ JNetPcap-Security Assignment/src/Scannerfinder.java
@@ -1,67 +1,47 @@
+package assignment4;
+
+import java.util.HashMap;
+import java.util.Map;
 import org.jnetpcap.Pcap;
-import org.jnetpcap.PcapIf;
-import org.jnetpcap.packet.PcapPacket;
-import org.jnetpcap.packet.PcapPacketHandler;
+import org.jnetpcap.packet.*;
 import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.tcpip.Http;
+import org.jnetpcap.protocol.tcpip.Tcp;
+import org.jnetpcap.packet.format.FormatUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+public class ScannerFinder {
 
-public class Scannerfinder
-{
-    public static void main(String[] args)
-    {
-        List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs
-        StringBuilder errbuf = new StringBuilder(); // For any error msgs
-        int r = Pcap.findAllDevs(alldevs, errbuf);
-        if (r != Pcap.OK || alldevs.isEmpty()) {
-            System.err.printf("Can't read list of devices, error is %s",
-                    errbuf.toString());
-            return;
-        }
-        System.out.println("Network devices found:");
-        int i = 0;
-        for (PcapIf device : alldevs) {
-            String description = (device.getDescription() != null) ? device
-                    .getDescription() : "No description available";
-            System.out.printf("#%d: %s [%s]\n", i++, device.getName(),
-                    description);
-        }
-        PcapIf device = alldevs.get(0); // Get first device in list
-        System.out.printf("\nChoosing '%s' on your behalf:\n",
-                (device.getDescription() != null) ? device.getDescription()
-                        : device.getName());
-        int snaplen = 64 * 1024; // Capture all packets, no trucation
-        int flags = Pcap.MODE_PROMISCUOUS; // capture all packets
-        int timeout = 10 * 1000; // 10 seconds in millis
-        Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
+    public static void main(String[] args) {
+
+        // StringBuilder is used to get error messages in case if any error occurs
+        StringBuilder errbuf = new StringBuilder();
+
+        // Make the pcap object
+        Pcap pcap = Pcap.openOffline(args[0], errbuf);
+
+        //If the pcap file does not exist, throw an error
         if (pcap == null) {
-            System.err.printf("Error while opening device for capture: "
-                    + errbuf.toString());
+            System.err.println(errbuf);
             return;
         }
-        PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
-            public void nextPacket(PcapPacket packet, String user) {
-                byte[] data = packet.getByteArray(0, packet.size()); // the package data
-                byte[] sIP = new byte[4];
-                byte[] dIP = new byte[4];
-                Ip4 ip = new Ip4();
-                if (packet.hasHeader(ip) == false) {
-                    return; // Not IP packet
-                }
-                ip.source(sIP);
-                ip.destination(dIP);
-                /* Use jNetPcap format utilities */
-                String sourceIP = org.jnetpcap.packet.format.FormatUtils.ip(sIP);
-                String destinationIP = org.jnetpcap.packet.format.FormatUtils.ip(dIP);
 
-                System.out.println("srcIP=" + sourceIP +
-                        " dstIP=" + destinationIP +
-                        " caplen=" + packet.getCaptureHeader().caplen());
+        Map<String, Integer> sendSyn = new HashMap<String, Integer>();
+        Map<String, Integer> synAck = new HashMap<String, Integer>();
+
+        pcap.loop(-1, new JPacketHandler<StringBuilder>() {
+
+            public void nextPacket(JPacket packet, StringBuilder errbuff) {
+                //Get relevant fields for the packet
+                Ip4 ip = new Ip4();
+                Tcp tcp = new Tcp();
+                Http http = new Http();
+
+                if (packet.hasHeader(ip)) {
+
+                }
             }
-        };
-        // capture first 10 packages
-        pcap.loop(10, jpacketHandler, "jNetPcap");
+        }, errbuf);
+
         pcap.close();
     }
 }

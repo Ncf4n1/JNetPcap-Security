9cb0641df24770137295adffd8e08c627ed8d84f
Logan Vining <llvinin@hotmail.com>
2019-03-26 16:30:08 -0600

    updated scanner finder

--- JNetPcap-Security Assignment/src/Scannerfinder.java
+++ JNetPcap-Security Assignment/src/Scannerfinder.java
@@ -1,5 +1,13 @@
-package assignment4;
+/*
+Computer Security Assignment 4
+March 26, 2019
+Team Members: Glen Johnson, Lizzy Hamaoka, Logan Vining
+*/
+package jnetpcap.security.assignment;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.jnetpcap.Pcap;
@@ -11,10 +19,10 @@ import org.jnetpcap.packet.format.FormatUtils;
 
 public class ScannerFinder {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Hashmaps to store the number of Syns sent and the number of SynAcks received
-        Map<String, Integer> sentSyn = new HashMap<String, Integer>();
-        Map<String, Integer> recSynAck = new HashMap<String, Integer>();
+        Map<String, Integer> sentSyn = new HashMap<>();
+        Map<String, Integer> recSynAck = new HashMap<>();
 
         // StringBuilder is used to get error messages in case if any error occurs
         StringBuilder errbuf = new StringBuilder();
@@ -30,6 +38,7 @@ public class ScannerFinder {
 
         pcap.loop(-1, new JPacketHandler<StringBuilder>() {
 
+            @Override
             public void nextPacket(JPacket packet, StringBuilder errbuff) {
                 //Get relevant fields for the packet
                 Ip4 ip = new Ip4();
@@ -76,14 +85,29 @@ public class ScannerFinder {
 
         pcap.close();
 
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        
+        writer.write("Computer Security Assignment 4\n");
+        writer.write("March 26, 2019\n");
+        writer.write("Team Members: Glen Johnson, Lizzy Hamaoka, Logan Vining\n");
+        writer.write("\n");
+        writer.write("\n");
+        writer.write("\n");
+        
         //Go through each ip in the sentSyn table
         for (String ip : sentSyn.keySet()) {
             //Check if that IP never received a SYN-ACK
-            if (recSynAck.get(ip) == null)
-                System.out.println(ip);
-            //Check if that IP sent 3 x as many SYN as SYN-ACK
-            else if (sentSyn.get(ip) >= (3 * recSynAck.get(ip)))
-                System.out.println(ip);
+            if (recSynAck.get(ip) != null)
+            {
+                //Check if that IP sent 3 x as many SYN as SYN-ACK
+                if (sentSyn.get(ip) >= (3 * recSynAck.get(ip)))
+                {
+                    writer.write(ip + "\n");
+                    System.out.println(ip);
+                }
+            }
         }
+        
+        writer.close();
     }
 }

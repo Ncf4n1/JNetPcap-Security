caaeb312be4a5e938fe664b448d54182f68e0b9c
Glen Johnson <glen.e.johnson@sbcglobal.net>
2019-03-26 12:05:19 -0600

    Finished?

--- JNetPcap-Security Assignment/src/Scannerfinder.java
+++ JNetPcap-Security Assignment/src/Scannerfinder.java
@@ -12,6 +12,9 @@ import org.jnetpcap.packet.format.FormatUtils;
 public class ScannerFinder {
 
     public static void main(String[] args) {
+        //Hashmaps to store the number of Syns sent and the number of SynAcks received
+        Map<String, Integer> sentSyn = new HashMap<String, Integer>();
+        Map<String, Integer> recSynAck = new HashMap<String, Integer>();
 
         // StringBuilder is used to get error messages in case if any error occurs
         StringBuilder errbuf = new StringBuilder();
@@ -25,9 +28,6 @@ public class ScannerFinder {
             return;
         }
 
-        Map<String, Integer> sendSyn = new HashMap<String, Integer>();
-        Map<String, Integer> synAck = new HashMap<String, Integer>();
-
         pcap.loop(-1, new JPacketHandler<StringBuilder>() {
 
             public void nextPacket(JPacket packet, StringBuilder errbuff) {
@@ -36,12 +36,54 @@ public class ScannerFinder {
                 Tcp tcp = new Tcp();
                 Http http = new Http();
 
+                //Check for both a tcp and ip connection
                 if (packet.hasHeader(ip)) {
-
+                    //Convert the source IP bytes to a string
+                    String ipAddress = FormatUtils.ip(ip.source());
+                    if (packet.hasHeader(tcp)) {
+                        //Check if syn packet
+                        //If syn flag is true and ack is false
+                        if (tcp.flags_SYN() == true && tcp.flags_ACK() == false){
+                            //Check if the ip is already in the hash
+                            Integer count = sentSyn.get(ipAddress);
+                            //If it doesn't exist, put it into the table with a count of 1 for the current packet
+                            if (count == null) {
+                                sentSyn.put(ipAddress, 1);
+                            }
+                            //If it does exist, increment the count by one for the current packet
+                            else {
+                                sentSyn.put(ipAddress, count + 1);
+                            }
+                        }
+                        //Check if SYN ACK packet
+                        //If syn flag and ack flag are true
+                        else if (tcp.flags_SYN() == true && tcp.flags_ACK() == true) {
+                            //Check if the ip is already in the hash
+                            Integer count = recSynAck.get(ipAddress);
+                            //If it doesn't exist, put it into the table with a count of 1 for the current packet
+                            if (count == null) {
+                                recSynAck.put(ipAddress, 1);
+                            }
+                            //If it does exist, increment the count by one for the current packet
+                            else {
+                                recSynAck.put(ipAddress, count + 1);
+                            }
+                        }
+                    }
                 }
             }
         }, errbuf);
 
         pcap.close();
+
+        //Go through each ip in the sentSyn table
+        for (String ip : sentSyn.keySet()) {
+            //Check if that IP never received a SYN-ACK
+            if (recSynAck.get(ip) == null)
+                System.out.println(ip);
+            //Check if that IP sent 3 x as many SYN as SYN-ACK
+            else if (sentSyn.get(ip) >= (3 * recSynAck.get(ip)))
+                System.out.println(ip);
+        }
     }
 }

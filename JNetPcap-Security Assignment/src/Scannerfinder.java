7926eec3682d08a7018b6cb801fdf6345927f012
Glen Johnson <glen.e.johnson@sbcglobal.net>
2019-03-26 18:32:07 -0600

    Update Scannerfinder.java

--- JNetPcap-Security Assignment/src/Scannerfinder.java
+++ JNetPcap-Security Assignment/src/Scannerfinder.java
@@ -19,7 +19,7 @@ import org.jnetpcap.packet.format.FormatUtils;
 
 public class ScannerFinder {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         //Hashmaps to store the number of Syns sent and the number of SynAcks received
         Map<String, Integer> sentSyn = new HashMap<>();
         Map<String, Integer> recSynAck = new HashMap<>();
@@ -47,12 +47,11 @@ public class ScannerFinder {
 
                 //Check for both a tcp and ip connection
                 if (packet.hasHeader(ip) && packet.hasHeader(tcp)) {
-                    //Convert the source IP bytes to a string
-                    String ipAddress = FormatUtils.ip(ip.source());
-
                     //Check if syn packet
                     //If syn flag is true and ack is false
                     if (tcp.flags_SYN() == true && tcp.flags_ACK() == false){
+                        //Convert the source IP bytes to a string
+                        String ipAddress = FormatUtils.ip(ip.source());
                         //Check if the ip is already in the hash
                         Integer count = sentSyn.get(ipAddress);
                         //If it doesn't exist, put it into the table with a count of 1 for the current packet
@@ -63,11 +62,14 @@ public class ScannerFinder {
                         else {
                             sentSyn.put(ipAddress, count + 1);
                         }
+                        System.out.println("Checked packet with SYN : " + ipAddress);
                     }
 
                     //Check if SYN ACK packet
                     //If syn flag and ack flag are true
                     else if (tcp.flags_SYN() == true && tcp.flags_ACK() == true) {
+                        //Convert the source IP bytes to a string
+                        String ipAddress = FormatUtils.ip(ip.destination());
                         //Check if the ip is already in the hash
                         Integer count = recSynAck.get(ipAddress);
                         //If it doesn't exist, put it into the table with a count of 1 for the current packet
@@ -78,7 +80,9 @@ public class ScannerFinder {
                         else {
                             recSynAck.put(ipAddress, count + 1);
                         }
+                        System.out.println("Checked packet with SYNACK : " + ipAddress);
                     }
+
                 }
             }
         }, errbuf);
@@ -86,28 +90,25 @@ public class ScannerFinder {
         pcap.close();
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-        
+
         writer.write("Computer Security Assignment 4\n");
         writer.write("March 26, 2019\n");
         writer.write("Team Members: Glen Johnson, Lizzy Hamaoka, Logan Vining\n");
         writer.write("\n");
         writer.write("\n");
         writer.write("\n");
-        
+
         //Go through each ip in the sentSyn table
         for (String ip : sentSyn.keySet()) {
             //Check if that IP never received a SYN-ACK
-            if (recSynAck.get(ip) != null)
-            {
-                //Check if that IP sent 3 x as many SYN as SYN-ACK
-                if (sentSyn.get(ip) >= (3 * recSynAck.get(ip)))
-                {
-                    writer.write(ip + "\n");
-                    System.out.println(ip);
-                }
-            }
+            if (recSynAck.get(ip) == null)
+                continue;
+            //Check if that IP sent 3 x as many SYN as SYN-ACK
+            else if (sentSyn.get(ip) >= (3 * recSynAck.get(ip)))
+                writer.write(ip + "\n");
+                System.out.println(ip);
         }
-        
+
         writer.close();
     }
 }
